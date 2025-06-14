4d2d514ee31ae23a5018d64533aeec90915d007f
RodrigoTrevmore <sosimousa@gmail.com>
2025-06-13 20:17:55 -0400

    first commit

--- final_project/index.js
+++ final_project/index.js
@@ -8,10 +8,72 @@ const app = express();
 
 app.use(express.json());
 
+const doesExist = (username) => {
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const authenticatedUser = (username, password) => {
+    // Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+    // Authenticate user
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
+});
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+    if (req.session.authorization) {
+        let token = req.session.authorization ['accessToken'];
+        jwt.verify(token, "access", (err, user) => {
+            if (!err){
+                req.user = user;
+                next();
+            } else {
+                res.status(403).json({message:"User Not Authenticated"});            
+            }
+        })
+    } else {
+        return res.status(403).json({message: "User not logged in"})
+    }
 });
  
 const PORT =5000;
