c2ba2828db4a875e7cc94d3bb65688315d8110c5
RodrigoTrevmore <sosimousa@gmail.com>
2025-06-13 21:15:31 -0400

    2nd commit

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "isbn" : "0001", "reviews": {} },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "isbn" : "0002", "reviews": {} },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "isbn" : "0003", "reviews": {} },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "isbn" : "0004", "reviews": {} },
+      5: {"author": "Unknown","title": "The Book Of Job", "isbn" : "0005", "reviews": {} },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "isbn" : "0006", "reviews": {} },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "isbn" : "0007", "reviews": {} },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "isbn" : "0008", "reviews": {} },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "isbn" : "0009", "reviews": {} },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "isbn" : "0010", "reviews": {} }
 }
 
 module.exports=books;
